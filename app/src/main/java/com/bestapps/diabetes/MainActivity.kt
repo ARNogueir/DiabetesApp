c214db2284cb3eb20db1b3da39b5d9dcb7503849
André Nogueira <anogueiracb@gmail.com>
2023-02-10 23:33:46 +0000

    first commit

--- app/src/main/java/com/bestapps/diabetes/MainActivity.kt
+++ app/src/main/java/com/bestapps/diabetes/MainActivity.kt
@@ -7,7 +7,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
