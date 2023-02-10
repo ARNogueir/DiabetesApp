78534ca1dee03d3df8ecb8941d0a597dd6a3f9ef
André Nogueira <anogueiracb@gmail.com>
2023-02-10 23:31:36 +0000

    first commit

--- app/src/main/java/com/bestapps/diabetes/MainActivity.kt
+++ app/src/main/java/com/bestapps/diabetes/MainActivity.kt
@@ -3,7 +3,6 @@ package com.bestapps.diabetes
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -11,11 +10,5 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val textView: TextView = findViewById(R.id.text)
-        textView.setOnClickListener {
-
-            textView.text = getString(R.string.name)
-        }
     }
 }
